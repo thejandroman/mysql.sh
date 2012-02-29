012c85432060fc7c68bce4e3b02be2ae0ebe8365
thejandroman <thejandroman@gmail.com>
2012-02-28 22:06:21 -0500

    Added return_error and finished v1 of mysql_connect
    Still need to QA.
    Should research potential for special characters in username and
    password and what steps should be taken to mitigate issues.
    Should there be some sort of server verification?

--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+function return_error {
+    >&2 printf "$1\n"
+    return $2
+}
+
 function mysql_connect {
-    local server
-    local port
-    local username
-    local password
-    server=$1
-    port=$2
-    username=$3
-    password=$4
+    local server=$1
+    if [ -n $1 ]; then return_error "Error: No server specified" 1; fi
+    local port=$2
+    if [ -z $port ]; then port=3306; fi
+    if ! [[ "$port" =~ ^[0-9]+$ ]] ; then
+        return_error "Error: Port not a number" 1
+        break
+    else
+        local username=$3
+        local password=$4
+    fi
+    printf "mysql -sN --host=$server --port=$port --user=$username --password=$password"
 }
