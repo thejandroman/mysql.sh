c047b84135138517014054f89f3a0ab88529cadf
thejandroman <thejandroman@gmail.com>
2012-03-25 11:09:48 -0400

    Added create table

--- test/provision.sql
+++ test/provision.sql
@@ -1,1 +1,8 @@
-create database mysqlshTest;
+CREATE DATABASE mysqlshTest;
+
+CREATE TABLE `unitTest1` (
+`id` INT (11) NOT NULL AUTO_INCREMENT,
+`string` VARCHAR(100) NOT NULL,
+`date` TEXT NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE = MyISAM DEFAULT CHARSET = latin1;
