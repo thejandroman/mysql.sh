d533f0ea0c4fe84494ccdf62ae1b3e026ebaeeec
thejandroman <thejandroman@gmail.com>
2012-03-25 12:11:24 -0400

    Debugging mysql_connect
    changed error returns

--- mysql.sh
+++ mysql.sh
@@ -14,10 +14,10 @@ function mysql_connect {
             u) local username="$OPTARG";;
             p) local password="$OPTARG";;
             S) disableSecureAuth="1";;
-            :) local rtrn=$(return_error "Error: -$OPTARG requires argument" 1)
-                return $rtrn;;
-            /?) local rtrn=$(return_error "Error: Incorrect option" 1)
-                return $rtrn;;
+            :) return_error "Error: -$OPTARG requires argument" 1
+                return $?;;
+            /?) return_error "Error: Incorrect option" 1
+                return $?;;
         esac
     done
     if [ "$disableSecureAuth" -eq "1" ]; then
@@ -28,8 +28,8 @@ function mysql_connect {
     if [ -z "$server" ]; then server="localhost"; fi
     if [ -z "$port" ]; then port="3306"; fi
     if ! [[ "$port" =~ ^[0-9]+$ ]] ; then
-        local rtrn=$(return_error "Error: Port not a number" 1)
-        return $rtrn
+        return_error "Error: Port not a number" 1
+        return $?
     fi
     if [ -z "$username" ]; then
         local userString=""
@@ -56,23 +56,23 @@ function mysql_query {
                 local varString="$OPTARG[*]"
                 local varArray=(${!varString})
                 IFS=$OLD_IFS;;
-            :) local rtrn=$(return_error "Error: -$OPTARG requires argument" 1)
-                return $rtrn;;
-            /?) local rtrn=$(return_error "Error: Incorrect option" 1)
-                return $rtrn;;
+            :) return_error "Error: -$OPTARG requires argument" 1
+                return $?;;
+            /?) return_error "Error: Incorrect option" 1
+                return $;;
         esac
     done
     if [ -z "$link" ]; then
-        local rtrn=$(return_error "Error: No link specified" 1)
-        return $rtrn
+        return_error "Error: No link specified" 1
+        return $?
     fi
     if [ -z "$query" ]; then
-        local rtrn=$(return_error "Error: No query specified" 1)
-        return $rtrn
+        return_error "Error: No query specified" 1
+        return $?
     fi
     varArray=($($link -B -e $query))
     if [ ! "$?" ]; then
-        local rtrn=$(return_error "Error: Query failed" 1)
-        return $rtrn
+        return_error "Error: Query failed" 1
+        return $?
     fi
 }
