fe05d68b763c3fcded1e29990a973e8b6bc692fb
Jandro <thejandroman@gmail.com>
2012-08-23 18:20:37 -0400

    Added some inserts to test

--- test/provision.sql
+++ test/provision.sql
@@ -1,8 +1,11 @@
-CREATE DATABASE mysqlshTest;
+CREATE DATABASE IF NOT EXISTS mysqlshTest;
 
-CREATE TABLE `unitTest1` (
+CREATE TABLE IF NOT EXISTS mysqlshTest.`unitTest1` (
 `id` INT (11) NOT NULL AUTO_INCREMENT,
 `string` VARCHAR(100) NOT NULL,
 `date` TEXT NOT NULL,
 PRIMARY KEY (`id`)
 ) ENGINE = MyISAM DEFAULT CHARSET = latin1;
+
+insert ignore into mysqlshTest.unitTest1 (string,date) VALUES ('this Has Spaces',NOW());
+insert ignore into mysqlshTest1.unitTest1 (string,date) VALUES ('thisHasNoSpaces',NOW());
