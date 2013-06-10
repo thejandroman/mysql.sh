79c9fad9d7c42b7960db2e5c29e7bf28c1494db0
Alejandro Figueroa <afigueroa@verisign.com>
2013-06-10 15:31:17 -0400

    Added GPLv3 license

--- build/build.sh
+++ build/build.sh
@@ -11,6 +11,23 @@ cd $functions
 allFunctions=$(ls .)
 
 echo "#!/bin/bash" > $mysqlSh
+cat <<EOF >> $mysqlSh
+
+#    Copyright (C) 2012,2013  Alejandro Figueroa
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+EOF
 for fnctn in $allFunctions; do
         echo >> $mysqlSh
         cat $fnctn >> $mysqlSh
